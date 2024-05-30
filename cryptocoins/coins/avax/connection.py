332b0767124de75c4ba4a00bd32b7b4b79a94909
gnutirt <ngtritung@gmail.com>
2024-05-30 09:32:04 +0700

    AT Fix Avax

--- cryptocoins/coins/avax/connection.py
+++ cryptocoins/coins/avax/connection.py
@@ -5,11 +5,11 @@ from web3.providers import (
 )
 
 
-def get_w3_won_connection():
+def get_w3_avax_connection():
     from exchange.settings import env
     w3 = Web3(HTTPProvider(env('AVAX_RPC_URL', default='https://avalanche-c-chain-rpc.publicnode.com')))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     return w3
 
 
-w3: Web3 = get_w3_won_connection()
+w3: Web3 = get_w3_avax_connection()
