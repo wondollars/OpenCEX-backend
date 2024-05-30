820c8bc96405294bd7c88b3112010bc09aedc1d5
gnutirt <ngtritung@gmail.com>
2024-05-30 13:26:58 +0700

    a

--- cryptocoins/data_sources/manager.py
+++ cryptocoins/data_sources/manager.py
@@ -27,44 +27,69 @@ class DataSourcesManager:
         for pair, price in new_data.items():
             external_exchanges_pairs_price_cache.set(pair.code, price)
 
-    def _get_source_data(self, source):
+    def _get_main_source_data(self):
         try:
-            return source.get_latest_prices()
+            return self.main_source.get_latest_prices()
         except Exception as e:
-            send_telegram_message(f'Datasource provider {source.NAME} error:\n{e}')
+            send_telegram_message(f'Datasource provider {self.main_source.NAME} error:\n{e}')
             return {}
 
-    def update_prices(self):
-        main_source_data = self._get_source_data(self.main_source)
-        all_sources_data = main_source_data.copy()
-
-        # Combine data from all reserve sources
+    def _get_reserve_source_data(self):
         for source in self.reserve_sources:
-            reserve_data = self._get_source_data(source)
-            all_sources_data.update(reserve_data)
+            try:
+                return source.get_latest_prices()
+            except Exception as e:
+                send_telegram_message(f'Datasource provider {source.NAME} error:\n{e}')
+        return {}
+
+    def update_prices(self):
+        self._get_main_source_data()
+        self._get_reserve_source_data()
+
+        main_source = self.main_source
+        reserve_sources = self.reserve_sources
 
         new_data: Dict[Pair, Decimal] = copy.copy(self._data)
 
+        # alerts
+        if not main_source.data:
+            for source in reserve_sources:
+                if source.data:
+                    main_source = source
+                    break
+            else:
+                send_telegram_message(f'All data sources not available!')
+                self._update_cached_prices()
+                return new_data
+
+        # check deviation
         for pair, old_price in self._data.items():
-            # Skip pairs with custom price
+            # skip pairs with custom price
             custom_price = PairSettings.get_custom_price(pair)
             if custom_price:
                 new_data[pair] = custom_price
                 continue
 
-            new_price = all_sources_data.get(pair.code)
+            new_price = main_source.data.get(pair)
             if new_price:
                 if not old_price:
                     new_data[pair] = new_price
                     continue
 
-                if calc_relative_percent_difference(old_price, new_price) < self.main_source.MAX_DEVIATION:
+                if calc_relative_percent_difference(old_price, new_price) < main_source.MAX_DEVIATION:
                     new_data[pair] = new_price
                 else:
-                    send_telegram_message(f'{pair.code} price changes more than {self.main_source.MAX_DEVIATION}%.'
-                                          f'\nCurrent price is {old_price}, new price: {new_price}')
+                    for source in reserve_sources:
+                        reserve_price = source.data.get(pair)
+                        if reserve_price and calc_relative_percent_difference(new_price, reserve_price) < source.MAX_DEVIATION:
+                            new_data[pair] = reserve_price
+                            break
+                    else:
+                        send_telegram_message(f'{pair.code} price changes more than {main_source.MAX_DEVIATION}%.'
+                                              f'\nCurrent price is {old_price}, new price: {new_price}')
             else:
-                send_telegram_message(f'No price available for {pair.code} in all sources.')
+                if PairSettings.is_alerts_enabled(pair):
+                    send_telegram_message(f'{main_source.NAME} {pair.code} price is not available!')
 
         from core.tasks.orders import run_otc_orders_price_update
         history = []
