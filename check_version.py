c83056cf752d3ee12902d2a5319df807a5d6f166
gnutirt <ngtritung@gmail.com>
2024-05-31 01:35:06 +0700

    Revert bot

--- check_version.py
+++ check_version.py
@@ -5,15 +5,26 @@ def check_versions(requirements_file):
         packages = file.readlines()
     
     for package in packages:
-        package_name = package.split('==')[0]
-        current_version = package.split('==')[1].strip()
+        if '==' in package:
+            package_name = package.split('==')[0]
+            current_version = package.split('==')[1].strip()
+        else:
+            package_name = package.strip()
+            current_version = "N/A"
+
+        try:
+            result = subprocess.run(
+                ['pip', 'index', 'versions', package_name],
+                capture_output=True, text=True
+            ).stdout.split('\n')
+            
+            # Find the line that contains "LATEST"
+            latest_version_line = next(line for line in result if "LATEST" in line)
+            latest_version = latest_version_line.split()[-1]
+            
+            print(f"{package_name}: current version: {current_version}, latest version: {latest_version}")
         
-        # Use pip to get the latest version of the package
-        latest_version = subprocess.run(
-            ['pip', 'index', 'versions', package_name],
-            capture_output=True, text=True
-        ).stdout.split('\n')[1].strip().split(' ')[-1]
-        
-        print(f"{package_name}: current version: {current_version}, latest version: {latest_version}")
+        except StopIteration:
+            print(f"Could not determine latest version for {package_name}")
 
 check_versions('requirements.txt')
