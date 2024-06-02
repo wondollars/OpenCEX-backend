8c3c0f279d78ba8e63774f43b95f9d9393cb75c1
gnutirt <ngtritung@gmail.com>
2024-06-02 22:24:51 +0700

    a

--- cryptocoins/coins/pepe/__init__.py
+++ cryptocoins/coins/pepe/__init__.py
@@ -5,7 +5,7 @@ PEPE = 39
 CODE = 'PEPE'
 DECIMALS = 18
 BLOCKCHAINS = {
-    'ETH': TokenParams(
+    'BNB': TokenParams(
         symbol=CODE,
         contract_address='0x6982508145454Ce325dDbE47a25d4ec3d2311933',
         decimal_places=18,
