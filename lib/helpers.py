894a8b65baae0de40517aa5ad328c0f93e5173b5
gnutirt <ngtritung@gmail.com>
2024-06-02 15:15:47 +0700

    to_de

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,162 +1,3 @@
-import binascii
-import datetime
-import decimal
-import hashlib
-import hmac
-import itertools
-import logging
-import math
-import time
-from decimal import Decimal, ROUND_DOWN, Context, localcontext
-from typing import Union
-
-from Crypto import Random
-from django.forms import model_to_dict
-from django.utils import timezone
-
-BOT_RE = "^bot[0-9]+@bot.com$"
-
-log = logging.getLogger(__name__)
-
-def dt_from_js(dt):
-    return datetime.datetime.fromtimestamp(dt / 1000, tz=timezone.utc)
-
-def remove_exponent(d: Decimal) -> Decimal:
-    """
-    Remove the exponent and trailing zeros from a Decimal
-    """
-    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
-
-def round_down(value, decimals):
-    with localcontext() as ctx:
-        d = to_decimal(value)
-        ctx.rounding = ROUND_DOWN
-        return round(d, decimals)
-
-def to_decimal(value, decimal_places: int = 18) -> Decimal:
-    """
-    Convert to decimal with rounding down
-    """
-    context = Context(prec=100)
-    value_as_str = str(value)
-    decimal_value = Decimal(value_as_str, context=context)
-    quantized_value = decimal_value.quantize(Decimal('1.' + '0' * decimal_places), rounding=ROUND_DOWN)
-    return quantized_value
-
-def pretty_decimal(number, digits=18) -> str:
-    """ returns formatted decimal number as string """
-    if number is None:
-        return None
-    number = to_decimal(number, decimal_places=digits)
-    return f'{{:0.{digits}f}}'.format(number).rstrip('0').rstrip('.')
-
-
-
-def to_decimal_pretty(number, digits=18, remove_exp=True):
-    res = to_decimal(pretty_decimal(number, digits=digits), decimal_places=digits)
-    return remove_exponent(res) if remove_exp else res
-
-def decimalize(val: Union[Decimal, str, int, float]) -> Decimal:
-    return Decimal(str(val))
-
-def round_by_precision(x, base, is_bid=True):
-    x = decimalize(x)
-    base = decimalize(base)
-    decimals = decimalize(10 ** base.as_tuple().exponent)
-    rounding = decimal.ROUND_FLOOR if is_bid else decimal.ROUND_CEILING
-    if base > 1:
-        return base * decimalize(x / base).quantize(decimals, rounding=rounding)
-    return decimalize(base * decimalize(x / base)).quantize(decimals, rounding=rounding)
-
-def normalize_data(data):
-    """
-    Cast specific data types to simple representation
-    """
-    if isinstance(data, dict):
-        for key, value in data.items():
-            # decimal
-            if isinstance(value, Decimal):
-                data[key] = float(value)
-
-            # recursive processing
-            if isinstance(value, (dict, list)):
-                data[key] = normalize_data(value)
-
-    elif isinstance(data, list):
-        result = []
-        for item in data:
-            result.append(normalize_data(item))
-
-        data = result
-
-    # decimal
-    elif isinstance(data, Decimal):
-        data = float(data)
-
-    return data
-
-def chunked(iterable, n, fill_value=None):
-    args = [iter(iterable)] * n
-    for i in itertools.zip_longest(fillvalue=fill_value, *args):
-        if tuple(i)[-1] == fill_value:
-            yield tuple(v for v in i if v != fill_value)
-        else:
-            yield i
-
-def make_hmac_signature_headers(api_key, secret_key):
-    nonce = str(int(time.time()))
-    message = api_key + nonce
-    signature = hmac.new(
-        secret_key.encode(),
-        message.encode('utf-8'),
-        hashlib.sha256
-    ).hexdigest().upper()
-    headers = {
-        'APIKEY': api_key,
-        'SIGNATURE': signature,
-        'NONCE': nonce
-    }
-    return headers
-
-def calc_absolute_percent_difference(num1, num2):
-    percent_diff = (max(num1, num2) - min(num1, num2)) / min(num1, num2) * 100
-    return percent_diff
-
-def calc_percent(num1, num2):
-    num1 = to_decimal(num1)
-    num2 = to_decimal(num2)
-    return (num2 - num1) / num1 * 100
-
-def calc_relative_percent_difference(num1, num2):
-    return math.fabs(calc_percent(num1, num2))
-
-def copy_instance(instance, model):
-    instance_data = model_to_dict(instance, fields=[
-        field.name for field in instance._meta.fields
-        if field.name != 'id'
-    ])
-
-    if instance_data.get('user') is not None:
-        instance_data['user_id'] = instance_data.get('user')
-        del instance_data['user']
-
-    return model(**instance_data)
-
-def get_iso_dt():
-    return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+'Z'
-
-def generate_unique_token(length=20):
-    return binascii.hexlify(Random.new().read(length)).decode('utf-8').upper()
-
-def find_similar_entry_by_field(field_name, field_value, entries_list):
-    for i, entry in enumerate(entries_list):
-        if entry.get(field_name) == field_value:
-            return i, entry
-    return None, None
-
-def sat_to_btc(sat):
-    return to_decimal(sat) / to_decimal(10**8)
-
 # import binascii
 # import datetime
 # import decimal
@@ -177,22 +18,20 @@ def sat_to_btc(sat):
 
 # log = logging.getLogger(__name__)
 
-
 # def dt_from_js(dt):
 #     return datetime.datetime.fromtimestamp(dt / 1000, tz=timezone.utc)
 
-
-# def remove_exponent(d: Decimal) -> Decimal: 
+# def remove_exponent(d: Decimal) -> Decimal:
+#     """
+#     Remove the exponent and trailing zeros from a Decimal
+#     """
 #     return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
-   
-
 
 # def round_down(value, decimals):
 #     with localcontext() as ctx:
 #         d = to_decimal(value)
 #         ctx.rounding = ROUND_DOWN
 #         return round(d, decimals)
-
 
 # def to_decimal(value, decimal_places: int = 18) -> Decimal:
 #     """
@@ -204,8 +43,7 @@ def sat_to_btc(sat):
 #     quantized_value = decimal_value.quantize(Decimal('1.' + '0' * decimal_places), rounding=ROUND_DOWN)
 #     return quantized_value
 
-
-# def pretty_decimal(number, digits=4) -> str:
+# def pretty_decimal(number, digits=18) -> str:
 #     """ returns formatted decimal number as string """
 #     if number is None:
 #         return None
@@ -213,15 +51,13 @@ def sat_to_btc(sat):
 #     return f'{{:0.{digits}f}}'.format(number).rstrip('0').rstrip('.')
 
 
-# def to_decimal_pretty(number, digits, remove_exp=True):
+
+# def to_decimal_pretty(number, digits=18, remove_exp=True):
 #     res = to_decimal(pretty_decimal(number, digits=digits), decimal_places=digits)
 #     return remove_exponent(res) if remove_exp else res
 
 # def decimalize(val: Union[Decimal, str, int, float]) -> Decimal:
 #     return Decimal(str(val))
- 
-
-
 
 # def round_by_precision(x, base, is_bid=True):
 #     x = decimalize(x)
@@ -229,9 +65,8 @@ def sat_to_btc(sat):
 #     decimals = decimalize(10 ** base.as_tuple().exponent)
 #     rounding = decimal.ROUND_FLOOR if is_bid else decimal.ROUND_CEILING
 #     if base > 1:
-#         return base*decimalize(x/base).quantize(decimals, rounding=rounding)
+#         return base * decimalize(x / base).quantize(decimals, rounding=rounding)
 #     return decimalize(base * decimalize(x / base)).quantize(decimals, rounding=rounding)
-
 
 # def normalize_data(data):
 #     """
@@ -260,7 +95,6 @@ def sat_to_btc(sat):
 
 #     return data
 
-
 # def chunked(iterable, n, fill_value=None):
 #     args = [iter(iterable)] * n
 #     for i in itertools.zip_longest(fillvalue=fill_value, *args):
@@ -268,7 +102,6 @@ def sat_to_btc(sat):
 #             yield tuple(v for v in i if v != fill_value)
 #         else:
 #             yield i
-
 
 # def make_hmac_signature_headers(api_key, secret_key):
 #     nonce = str(int(time.time()))
@@ -285,21 +118,17 @@ def sat_to_btc(sat):
 #     }
 #     return headers
 
-
 # def calc_absolute_percent_difference(num1, num2):
 #     percent_diff = (max(num1, num2) - min(num1, num2)) / min(num1, num2) * 100
 #     return percent_diff
 
-
 # def calc_percent(num1, num2):
 #     num1 = to_decimal(num1)
 #     num2 = to_decimal(num2)
-#     return (num2 - num1)/num1 * 100
-
+#     return (num2 - num1) / num1 * 100
 
 # def calc_relative_percent_difference(num1, num2):
 #     return math.fabs(calc_percent(num1, num2))
-
 
 # def copy_instance(instance, model):
 #     instance_data = model_to_dict(instance, fields=[
@@ -313,14 +142,11 @@ def sat_to_btc(sat):
 
 #     return model(**instance_data)
 
-
 # def get_iso_dt():
 #     return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+'Z'
 
-
 # def generate_unique_token(length=20):
 #     return binascii.hexlify(Random.new().read(length)).decode('utf-8').upper()
-
 
 # def find_similar_entry_by_field(field_name, field_value, entries_list):
 #     for i, entry in enumerate(entries_list):
@@ -328,6 +154,180 @@ def sat_to_btc(sat):
 #             return i, entry
 #     return None, None
 
-
 # def sat_to_btc(sat):
 #     return to_decimal(sat) / to_decimal(10**8)
+
+import binascii
+import datetime
+import decimal
+import hashlib
+import hmac
+import itertools
+import logging
+import math
+import time
+from decimal import Decimal, ROUND_DOWN, Context, localcontext
+from typing import Union
+
+from Crypto import Random
+from django.forms import model_to_dict
+from django.utils import timezone
+
+BOT_RE = "^bot[0-9]+@bot.com$"
+
+log = logging.getLogger(__name__)
+
+
+def dt_from_js(dt):
+    return datetime.datetime.fromtimestamp(dt / 1000, tz=timezone.utc)
+
+
+def remove_exponent(d: Decimal) -> Decimal: 
+    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
+   
+
+
+def round_down(value, decimals):
+    with localcontext() as ctx:
+        d = to_decimal(value)
+        ctx.rounding = ROUND_DOWN
+        return round(d, decimals)
+
+
+def to_decimal(value, decimal_places: int = 18) -> Decimal:
+    """
+    Convert to decimal with rounding down
+    """
+    context = Context(prec=100)
+    value_as_str = str(value)
+    decimal_value = Decimal(value_as_str, context=context)
+    quantized_value = decimal_value.quantize(Decimal('1.' + '0' * decimal_places), rounding=ROUND_DOWN)
+    return quantized_value
+
+
+def pretty_decimal(number, digits=18) -> str:
+    """ returns formatted decimal number as string """
+    if number is None:
+        return None
+    number = to_decimal(number, decimal_places=digits)
+    return f'{{:0.{digits}f}}'.format(number).rstrip('0').rstrip('.')
+
+
+def to_decimal_pretty(number, digits, remove_exp=True):
+    res = to_decimal(pretty_decimal(number, digits=digits), decimal_places=digits)
+    return remove_exponent(res) if remove_exp else res
+
+def decimalize(val: Union[Decimal, str, int, float]) -> Decimal:
+    return Decimal(str(val))
+ 
+
+
+
+def round_by_precision(x, base, is_bid=True):
+    x = decimalize(x)
+    base = decimalize(base)
+    decimals = decimalize(10 ** base.as_tuple().exponent)
+    rounding = decimal.ROUND_FLOOR if is_bid else decimal.ROUND_CEILING
+    if base > 1:
+        return base*decimalize(x/base).quantize(decimals, rounding=rounding)
+    return decimalize(base * decimalize(x / base)).quantize(decimals, rounding=rounding)
+
+
+def normalize_data(data):
+    """
+    Cast specific data types to simple representation
+    """
+    if isinstance(data, dict):
+        for key, value in data.items():
+            # decimal
+            if isinstance(value, Decimal):
+                data[key] = float(value)
+
+            # recursive processing
+            if isinstance(value, (dict, list)):
+                data[key] = normalize_data(value)
+
+    elif isinstance(data, list):
+        result = []
+        for item in data:
+            result.append(normalize_data(item))
+
+        data = result
+
+    # decimal
+    elif isinstance(data, Decimal):
+        data = float(data)
+
+    return data
+
+
+def chunked(iterable, n, fill_value=None):
+    args = [iter(iterable)] * n
+    for i in itertools.zip_longest(fillvalue=fill_value, *args):
+        if tuple(i)[-1] == fill_value:
+            yield tuple(v for v in i if v != fill_value)
+        else:
+            yield i
+
+
+def make_hmac_signature_headers(api_key, secret_key):
+    nonce = str(int(time.time()))
+    message = api_key + nonce
+    signature = hmac.new(
+        secret_key.encode(),
+        message.encode('utf-8'),
+        hashlib.sha256
+    ).hexdigest().upper()
+    headers = {
+        'APIKEY': api_key,
+        'SIGNATURE': signature,
+        'NONCE': nonce
+    }
+    return headers
+
+
+def calc_absolute_percent_difference(num1, num2):
+    percent_diff = (max(num1, num2) - min(num1, num2)) / min(num1, num2) * 100
+    return percent_diff
+
+
+def calc_percent(num1, num2):
+    num1 = to_decimal(num1)
+    num2 = to_decimal(num2)
+    return (num2 - num1)/num1 * 100
+
+
+def calc_relative_percent_difference(num1, num2):
+    return math.fabs(calc_percent(num1, num2))
+
+
+def copy_instance(instance, model):
+    instance_data = model_to_dict(instance, fields=[
+        field.name for field in instance._meta.fields
+        if field.name != 'id'
+    ])
+
+    if instance_data.get('user') is not None:
+        instance_data['user_id'] = instance_data.get('user')
+        del instance_data['user']
+
+    return model(**instance_data)
+
+
+def get_iso_dt():
+    return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+'Z'
+
+
+def generate_unique_token(length=20):
+    return binascii.hexlify(Random.new().read(length)).decode('utf-8').upper()
+
+
+def find_similar_entry_by_field(field_name, field_value, entries_list):
+    for i, entry in enumerate(entries_list):
+        if entry.get(field_name) == field_value:
+            return i, entry
+    return None, None
+
+
+def sat_to_btc(sat):
+    return to_decimal(sat) / to_decimal(10**8)
