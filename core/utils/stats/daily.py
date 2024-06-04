833035781411deb13576e6a1d7ce896a18c202c7
gnutirt <ngtritung@gmail.com>
2024-06-04 15:07:02 +0700

    Test

--- core/utils/stats/daily.py
+++ core/utils/stats/daily.py
@@ -47,9 +47,9 @@ def get_last_prices(ts=None):
 
     
     # test = {}
-    # test = get_last_price_from_otc()
+    test = get_last_price_from_otc()
     send_telegram_message(f'resultq: {resultq}')
-    # send_telegram_message(f'resultq_: {test}')
+    send_telegram_message(f'test: {test}')
     return resultq
 
 def get_last_price_from_otc(): 
